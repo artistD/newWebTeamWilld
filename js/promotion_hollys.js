17a9e2fdd15d5b0c13dee07e1ca0d9f89219c74f
mky1428 <mky1428@gmail.com>
2021-11-01 16:35:33 +0900

    게시글 작업1

---
+++ js/promotion_hollys.js
@@ -0,0 +1,7 @@
+function eventmove1(){
+    window.location.href = "https://www.hollys.co.kr/news/event/view.do?idx=279&pageNo=1&division=";
+}
+
+function eventmove2(){
+    window.location.href = "https://www.hollys.co.kr/news/event/view.do?idx=276&pageNo=1&division=";
+}
---
+++ js/promotion_starbucks.js
@@ -0,0 +1,3 @@
+function eventmove(){
+    window.location.href = "https://www.starbucks.co.kr/whats_new/store_event_list.do";
+}
